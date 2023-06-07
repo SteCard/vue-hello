0243230e778d8507ef8c8f54893a815d3ba8519d
SteCard <stefanocardillo14@gmail.com>
2023-06-07 20:07:27 +0200

    Testo stampato a schermo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// RICHIAMO FUNZIONE CREATE_APP VUE.JS E LA INSERISCO IN UNA CONSTANTE
+const {createApp} = Vue;
+
+// FUNZIONE CREATE_APP
+createApp({
+
+    data(){
+
+        return{
+            msg : "久しぶりだな"
+        }
+    }
+}).mount('#app'); // RICHIAMO ID DOM 
